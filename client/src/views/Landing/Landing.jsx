f1df4c88e549e09d1ebaf3e38305740b7dbe5982
agustin-vacca <112440788+agustin-vacca@users.noreply.github.com>
2023-05-23 17:29:00 -0300

    fixed styles and new picture

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -16,19 +16,20 @@ const Landing = () => {
   ];
 
   return (
-    <div name="landing" className="w-full h-screen bg-green-800">
+    <div name="landing" className="w-full min-h-screen bg-green-800">
       <h2 className="flex px-2 py-2 text-2xl text-white font-serif"> HuntTravel </h2>
 
-      <div className="max-w-[9000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <h2 className="text-center text-4xl m-3 text-lime-200 font-serif">
+      <div className="max-w-[9000px] mx-auto px-8 flex flex-col justify-center h-max">
+        <h2 className="text-center text-4xl my-12 text-lime-200 font-serif">
           {" "}
           SEARCH YOUR ACTIVITY{" "}
         </h2>
-        <ImgSlider className="justify-center p-3" slides={slides} />
+        <ImgSlider className="justify-center p-3 h-max" slides={slides} />
 
-        <div>
+        <div className="my-10">
           <Link to="/home">
-            <button className=" font-serif bg-lime-700 text-white mt-4 border-2 px-6 py-3 my-8 flex items-center hover:bg-amber-400 hover:border-amber-400">
+            <button className=" font-serif bg-lime-700 text-white mt-4 border-2 px-6 py-3 my-8 flex items-center hover:bg-amber-400 hover:border-amber-400
+            active:transform active:translate-y-1 transition-all">
               {" "}
               Welcome{" "}
             </button>
